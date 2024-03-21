5f807f1b2235cd131c8aabca44064052727b0da2
ct890505 <120002632+ct890505@users.noreply.github.com>
2024-03-21 10:34:43 +0700

    Add files via upload

--- tim-as.py
+++ tim-as.py
@@ -2,21 +2,23 @@
 from cymruwhois import Client
 import xlrd
 import openpyxl
-import pygeoip
+import requests
+import colorama
+from colorama import *
 
-print ('''
-\033[1;33m
+print (f'''
+{Fore.BLUE}
                 $$$$$$\ $$$$$$$\        $$$$$$$$\ $$$$$$$\   $$$$$$\   $$$$$$\  $$\   $$\ $$$$$$$$\ $$$$$$$\$
                 \_$$  _|$$  __$$\       \__$$  __|$$  __$$\ $$  __$$\ $$  __$$\ $$ | $$  |$$  _____|$$  __$$\$
                   $$ |  $$ |  $$ |         $$ |   $$ |  $$ |$$ /  $$ |$$ /  \__|$$ |$$  / $$ |      $$ |  $$ |
                   $$ |  $$$$$$$  |         $$ |   $$$$$$$  |$$$$$$$$ |$$ |      $$$$$  /  $$$$$\    $$$$$$$  |
-                  $$ |  $$  ____/\033[1;33m          $$ |   $$  __$$< $$  __$$ |$$ |      $$  $$<   $$  __|   $$  __$$<
-                  $$ |  $$ |               $$ |   $$ |  $$ |$$ |  $$ |$$ |  $$\ $$ |\$$\  $$ |      $$ |  $$ |\033[1;31m{v1.0}\033[1;31m
+                  $$ |  $$  ____/{Fore.GREEN}          $$ |   $$  __$$< $$  __$$ |$$ |      $$  $$<   $$  __|   $$  __$$<
+                  $$ |  $$ |               $$ |   $$ |  $$ |$$ |  $$ |$$ |  $$\ $$ |\$$\  $$ |      $$ |  $$ |\033[1;31m--v1.0--\033[1;31m
                 $$$$$$\ $$ |               $$ |   $$ |  $$ |$$ |  $$ |\$$$$$$  |$$ | \$$\ $$$$$$$$\ $$ |  $$ |
                 \______|\__|               \__|   \__|  \__|\__|  \__| \______/ \__|  \__|\________|\__|  \__|\033[1;32m
 \033[1;33m
 ''')
-print("\033[1;33m==================================================================================================\033[1;33m \033[1;33m CREAT BY CT89")
+print(f"{Fore.LIGHTCYAN_EX}=================================================================================================={Fore.LIGHTGREEN_EX} CREAT BY CT89")
 
 def ip_1():
 
@@ -27,6 +29,7 @@ def ip_1():
         c = Client()
         r= c.lookup(IP)
         print('AS '+ r.asn + ' NAME: '+ r.owner )
+        track_location2(IP)
     if select == '2':
         file_exel = 'Book1.xlsx'
         wb = xlrd.open_workbook_xls(file_exel)
@@ -65,12 +68,56 @@ def ip_2():
                     if index !=-1 or index2!=-1:          #'VINAGAME-AS-VN VNG Corporation, VN' or 'FACEBOOK, US'
                         f.write(row[1] + " -- " + r.asn + " --- " + r.owner + "\n")
                         f.close
-def track_location():
-    gip = pygeoip.GeoIP("GeoLiteCity.dat")
-    res=gip.record_by_addr('Enter the ip')
-    for key,val in res.items():
-        print('%s : %s' % (key,val))
+def track_location(ip):
+    ip_address = ip
+    response = requests.get(f'https://api.iplocation.net/?ip={ip_address}')
+    print(response)
+    data = response.json()
+    location_data = {
+                    "Ip Address":ip_address,
+                    "city": data.get("city"),
+                    "ip_number": data.get("ip_number"),
+                    "ip_version": data.get("ip_version"),
+                    "country": data.get("country_name"),
+                    "country_code2": data.get("country_code2"),
+                    "isp": data.get("isp"),
+                    "Browser": data.get("browser"),
+                   
+                }
+    print(Fore.LIGHTGREEN_EX + str(location_data))
 
+def track_location2(ip):
+    ip_address = ip
+    response = requests.get(f'http://ip-api.com/json/{ip_address}')
+    print(response)
+    data = response.json()
+    location_data = {
+            "IP": data.get("query"),
+            "status": data.get("status"),
+            "continent": data.get("continent"),
+            "continentCode": data.get("continentCode"),
+            "country": data.get("country"),
+            "countryCode": data.get("countryCode"),
+            "region": data.get("region"),
+            "regionName": data.get("regionName"),
+            "city": data.get("city"),
+            "district":data.get("district"),
+            "zip": data.get("zip"),
+            "lat": data.get("lat"),
+            "lon": data.get("lon"),
+            "timezone": data.get("timezone"),
+            "offset": data.get("offset"),
+            "currency": data.get("currency"),
+            "isp": data.get("isp"),
+            "org": data.get("org"),
+            "as": data.get("as"),
+            "asname": data.get("asname"),
+            "mobile":data.get("mobile"),
+            "proxy": data.get("proxy"),
+            "hosting": data.get("hosting")
+        }
+    print(Fore.LIGHTGREEN_EX + str(location_data))
 
 if __name__=="__main__":
-   ip_2()
+    ip_1()
+    # track_location2("117.2.91.179")
